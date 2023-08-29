dee4f815308b798bf73445ae040bdaee85d12e1a
Saeed Rasooli <saeed.gnu@gmail.com>
2023-08-29 15:06:24 +0330

    LogstashClient: remove sendDjangoRequest, and rename a method

--- logstash/client.py
+++ logstash/client.py
@@ -27,7 +27,7 @@ class LogstashClient(DatagramClient):
         DatagramClient.__init__(self, host=host, port=port)
 
     def sendDict(self, message: "dict[str, Any]"):
-        message["@timestamp"] = self.current_timestamp()
+        message["@timestamp"] = self._currentTimestamp()
         message["@version"] = "1"
         if "tags" not in message:
             message["tags"] = self.tags
@@ -37,39 +37,7 @@ class LogstashClient(DatagramClient):
             json.dumps(message, default=str).encode("utf-8"),
         )
 
-    def current_timestamp(self):
+    def _currentTimestamp(self):
         dt = datetime.utcnow().replace(tzinfo=None)
         return dt.strftime("%Y-%m-%dT%H:%M:%S") + \
             ".%03d" % (dt.microsecond / 1000) + "Z"
-
-    def sendDjangoRequest(self, req, **kwargs):
-        from ipware import get_client_ip
-        # kwargs can have "pk"
-        remote_ip, _is_routable = get_client_ip(req)
-        host = req.get_host()
-        host_parts = host.split(":")
-        host_ip = host_parts[0]
-        host_dict = {
-            "ip": host_ip,
-        }
-        if len(host_parts) > 1:
-            host_dict["port"] = host_parts[1]
-
-        # keys should not start with "@" except timestamp and version
-        message = {
-            "message": f"{req.method} {req.path}",
-            "host": host_dict,
-            "path": req.path,
-            "level": "INFO",
-            "logger_name": "django.requests",
-            # extra fields:
-            "username": req.user.username,
-            "remote_ip": remote_ip,
-            "method": req.method,
-            # "request": req.__dict__,
-        }
-        for key, value in kwargs.items():
-            # map "pk" to "object_id" ?
-            message[key] = value
-
-        self.sendDict(message)
